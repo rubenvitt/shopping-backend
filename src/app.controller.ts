1c8650f8a94bacbc790642878f834c1513b687c1
Ruben Vitt <r.vitt@fme.de>
2020-10-08 15:05:31 +0200

    :sparkles:(version-status): add version- & status api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,18 @@
-import { Controller} from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
 
 @Controller()
 export class AppController {
+    @Get('/')
+    mainRoute() {
+        return {
+            status: 'RUNNING'
+        }
+    }
 
+    @Get('/api/version')
+    apiVersion() {
+        return {
+            apiVersion: '1.0'
+        }
+    }
 }
